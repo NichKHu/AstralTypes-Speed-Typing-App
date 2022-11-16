7294691b3954e77940bcc8f2acdc95ba95192bd8
Aaron H <94340638+AaronKHu@users.noreply.github.com>
2022-11-15 19:18:46 -0800

    Seperated each words with divs

--- js/main.js
+++ js/main.js
@@ -6,9 +6,15 @@ function randomWord() {
     return words[randomIndex];
 }
 
+function formatWord(word) {
+    return `<div class="word">${word}</div>`;
+}
+
 function newGame() {
     document.getElementById('words').innerHTML = '';
     for (let i=0; i < 200; i++) {
-
+        document.getElementById('words').innerHTML += formatWord(randomWord());
     }
 }
+
+newGame();
